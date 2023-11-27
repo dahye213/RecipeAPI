53a22dcab2921bd50ffeb5c8154a98c3521bde58
DragoScript <94208742+DragoScript@users.noreply.github.com>
2023-11-26 21:24:37 -0500

    Added 3 new files to RecipeAPI
    IRateRepository
    RateRepository
    Rate Controller

---
+++ FoodRecipeAPI/Interfaces/IRateRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using FoodRecipeAPI.Models;
+
+namespace FoodRecipeAPI.Interfaces
+{
+	public interface IRateRepository {
+        ICollection<Rate> GetAllRate();
+        Rate GetRate(int id);
+        Rate GetRate(decimal score);
+        Rate GetRate(string comment);
+        decimal GetRateRecipe(int RecipeId);
+        bool RateExists(int id);
+    }
+}
