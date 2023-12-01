d1ff86fd6206e4f9cae084003427878ddc2c4e21
dahye213 <113384557+dahye213@users.noreply.github.com>
2023-12-01 16:56:34 -0500

    Setting up RDS database

--- FoodRecipeAPI/Program.cs
+++ FoodRecipeAPI/Program.cs
@@ -27,7 +27,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<DataContext>(options =>
 {
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Connection2RDS"));
 });
 
 var app = builder.Build();
